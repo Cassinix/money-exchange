3c35f8b4dc30c61a67360eeb35054e39f38b3a4c
Cassinix <ninicassini@gmail.com>
2019-03-16 23:07:50 +0300

    feat: implement function makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 // PLEASE DON'T change function name
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    const result = {};
+    //const dollars = [50, 25, 10, 5, 1];
+    /*
+    const calculate = (n) => {
+        return (currency - (currency % n)) / n;
+    }
+    */
+    const calc = (currency) => {
+    if (currency > 1000) {
+        result.error = "You are rich, my friend! We don't have so much coins for exchange"
+        return result;
+    }
+    else if (currency >= 50 && currency <= 1000) {
+        result.H = (currency - (currency % 50)) / 50;
+        return calc(currency % 50);
+    }
+    
+    else if (currency >= 25 && currency < 50) {
+        result.Q = (currency - (currency % 25)) / 25;
+        return calc(currency % 25);
+    }
+    else if (currency >= 10 && currency < 25) {
+        result.D = (currency - (currency % 10)) / 10;
+        return calc(currency % 10);
+    }
+    else if (currency >= 5 && currency < 25) {
+        result.N = (currency - (currency % 5)) / 5;
+        return calc(currency % 5);
+    }
+    else if (currency >= 1 && currency < 5) {
+        result.P = (currency - (currency % 1)) / 1;
+        return calc(currency % 1);
+    }
+    return result;
+    }
+    return calc(currency);
 }
